ad6b01a00a155ad758a847519c983d75396f23a1
kafadarova <k.kafadarova@gmail.com>
2018-05-11 23:07:19 +0200

    added event listerner for the keypress + button clicked

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
+//BUDGET CONTROLLER
 //creating a controller module using IIEF - immediately invoked expression function
 //IIFE allow data pravicy because it creates a new scope that is not visible from outside scope
 var budgetController = (function() {
-  var x = 23;
-
-  var add = function(a) {
-    return a + x;
-  }
-
-  return {
-    publicTest: function(b)  {
-      return add(b);
-    }
-  }
+  // Some code
 })();
 
 //module = function expression
 
-//module
-var UIController = (function(){
+//UI CONTROLLER
+var UIController = (function() {
   //Some code
 })();
 
-//module
-var controller = (function(budgetCtrl, UICtrl){
-   var z = budgetCtrl.publicTest(5);
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
 
-   return {
-     anotherPublic: function(){
-       console.log(z);
-     }
-   }
+  var ctrlAddItem = function(){
+    // 1. Get the field input data
+    // 2. Add the tem to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget
+  }
+
+  //event listener for input button
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  //keypress event doesnt happen on any specific element, but on the global web page (anywhere on the document)
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
