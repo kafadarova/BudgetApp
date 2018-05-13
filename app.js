7bff119fb75d137ee60fb17c51b52a6887a0f724
kafadarova <k.kafadarova@gmail.com>
2018-05-13 11:38:00 +0200

    create an initialization function

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ var budgetController = (function() {
 var UIController = (function() {
   var DOMstrings = {
     inputType: '.add__type',
-    inputDescription : '.add__description',
+    inputDescription: '.add__description',
     inputValue: '.add__value',
     inputBtn: '.add__btn'
   }
   return {
     getinput: function() {
       return {
-      //will be either inc or exp
-      type: document.querySelector(DOMstrings.inputType).value,
-      description: document.querySelector(DOMstrings.inputDescription).value,
-      value: document.querySelector(DOMstrings.inputValue).value
+        //will be either inc or exp
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
     },
     //exposing the DOMstrings to the public
@@ -33,25 +33,43 @@ var UIController = (function() {
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  //get the DOM strings and stored them into the var called DOM
-  var DOM = UICtrl.getDOMstrings();
+
+  //set up an event listeners
+  var setupEventListeners = function() {
+
+    //get the DOM strings and stored them into the var called DOM
+    var DOM = UICtrl.getDOMstrings();
+
+    //event listener for input button
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    //keypress event doesnt happen on any specific element, but on the global web page (anywhere on the document)
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
 
   var ctrlAddItem = function() {
     // 1. Get the field input data
     var input = UICtrl.getinput();
-    console.log(input);
+
     // 2. Add the tem to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget
-  }
+  };
 
-  //event listener for input button
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-  //keypress event doesnt happen on any specific element, but on the global web page (anywhere on the document)
-  document.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  //public function - so we will return it into an object
+  return {
+    init: function() {
+      console.log('Application has started');
+      setupEventListeners();
     }
-  });
+  };
+
 })(budgetController, UIController);
+
+//execute rigth at the begging when our app has started
+controller.init();
